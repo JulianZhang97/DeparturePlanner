8c39ad55557985c96098c542b9310db9ba484f3e
Julian Zhang <julian.zhang@mail.utoronto.ca>
2019-02-03 16:02:11 -0500

    Code cleanup

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -4,14 +4,6 @@ import Routes from './Routes.js';
 
 
 class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    // fetch('/users')
-    //   .then(res => res.json())
-    //   .then(users => this.setState({ users }));
-  }
-
   render() {
     return (
       <div>
